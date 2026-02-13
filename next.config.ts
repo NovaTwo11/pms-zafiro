0c2bca13f11cf0f37c64fd551993f394d3b60f5f
Holdan Lopez <holdann.lopezs@gmail.com>
2026-02-13 10:51:55 -0500

    Integración Lógica con Back 16 (En proceso) v6.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**', // El doble asterisco permite cualquier dominio seguro
+            },
+            {
+                protocol: 'http',
+                hostname: '**', // Opcional: Permite HTTP local o sin certificado
+            }
+        ],
+    },
 };
 
 export default nextConfig;
